37a97caa761ab9e728c74f793ab834a83540624e
HAN <reyhanrafaidhil1126@gmail.com>
2026-08-14 20:40:24 +0700

    feat: add date detection using regex

--- backend/app/services/detection_service.py
+++ backend/app/services/detection_service.py
@@ -1,5 +1,6 @@
 from app.services.detectors.regex.phone_detector import detect_phone
 from app.services.detectors.regex.email_detector import detect_email
+from app.services.detectors.regex.date_detector import detect_date
 
 class DetectionService:
 
@@ -14,9 +15,11 @@ class DetectionService:
 
         phone_entities = detect_phone(text)
         email_entities = detect_email(text)
+        date_entities = detect_date(text)
 
         entities.extend(phone_entities)
         entities.extend(email_entities)
+        entities.extend(date_entities)
 
         # =========================
         # PRESIDIO DETECTION
