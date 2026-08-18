40ee75cb825776197b0d317e45f2b38eb9528adf
Gaurav Kumar <78421549+Gaurav167hue@users.noreply.github.com>
2026-08-18 09:41:53 +0530

    Update roolback.py

--- firmware/roolback.py
+++ firmware/roolback.py
@@ -1,1 +1,11 @@
+from .version import parse_version
 
+
+def allow_firmware_update(current_version, new_version):
+    current = parse_version(current_version)
+    new = parse_version(new_version)
+
+    if new <= current:
+        return False
+
+    return True
